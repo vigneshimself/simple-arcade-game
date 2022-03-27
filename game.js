7c49f9b3a70eddcfdcfe30b8550d2f8110930476
vignesh <40684259+vigneshimself@users.noreply.github.com>
2022-03-27 19:20:08 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,41 +1,42 @@
+//let codey;
 const gameState = {};
 
-
-
 function preload() {
-  this.load.image('codey', '/codey.png');
+  this.load.image("codey", "/bot.png");
 }
 
 function create() {
-  gameState.codey = this.add.sprite(40, 200, 'codey');
+  gameState.codey = this.add.sprite(30, 200, "codey");
   gameState.cursors = this.input.keyboard.createCursorKeys();
 }
 
+// Create your update() function here
 function update() {
-	// Update based on keypress here!
-   if (gameState.cursors.right.isDown) {
-	 gameState.codey.x += 5;
-   }
-   if (gameState.cursors.left.isDown) {
-	 gameState.codey.x -= 5;
-   }
-   if (gameState.cursors.up.isDown) {
-	 gameState.codey.y += 5;
-   }
-   if (gameState.cursors.down.isDown) {
-	 gameState.codey.y -= 5;
+  if (gameState.cursors.right.isDown) {
+    gameState.codey.x += 5;
   }
-
-const config = {
-	type: Phaser.AUTO,
-	width: 400,
-	height: 400,
-	backgroundColor: "#5f2a55",
-	scene: {
-    preload,
-    create,
-    update
-	}
+  if (gameState.cursors.left.isDown) {
+    gameState.codey.x -= 5;
+  }
+  if (gameState.cursors.up.isDown) {
+    gameState.codey.y -= 5;
+  }
+  if (gameState.cursors.down.isDown) {
+    gameState.codey.y += 5;
+  }
 }
 
-const game = new Phaser.Game(config)
+const config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 400,
+  backgroundColor: "#5f2a55",
+  scene: {
+    preload,
+    create,
+    update,
+    // Include update here!
+  },
+};
+
+const game = new Phaser.Game(config);
